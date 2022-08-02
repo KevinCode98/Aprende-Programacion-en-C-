5dc6f731241809b2ca253d1fb5d4fa57b13441c3
KevinCode98 <kevincode98@gmail.com>
2022-08-01 22:10:24 -0500

    Seccion 1 -> Terminada el 01/08/22 -> Primer programa, Tipos de datos y Entrada y salida de datos por consola.

---
+++ S1_Introduccion_EntradaYSalidas/Lectura_EntradaDeDatos.cpp
@@ -0,0 +1,15 @@
+// Lectura o entrada de datos
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  float numero; // Definiendo la variable
+
+  cout << "Digite un nuemero: ";
+  cin >> numero;
+  cout << "\nEl numero que digito es: " << numero << endl;
+
+  return 0;
+}
